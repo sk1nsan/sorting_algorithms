e3a239500a9900c3c08ec30e04a1ca3f38d6650e
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-02-16 01:26:19 +0200

    bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,22 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j, temp;
+
+    for (i = 0; i  < size - 1; i++)
+    {
+        for (j = 0; j < size - 1;  j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+
+            }
+
+        }
+    }
+}
