4ce1c8962296998222065fb0366e93a9d6d7dd31
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-02-16 01:30:25 +0200

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,22 +1,28 @@
 #include "sort.h"
+/**
+ * bubble_sort - sorting using bubble algo
+ *
+ * @array: array to be sorted
+ * @size: size of array
+ *
+ * Return: Nothing
+*/
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j, temp;
+	size_t i, j, temp;
 
-    for (i = 0; i  < size - 1; i++)
-    {
-        for (j = 0; j < size - 1;  j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-
-            }
-
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
