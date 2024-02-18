8c17d100007e3fe4d5b19be5ae7ba1642b0932f7
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-02-18 20:09:15 +0200

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,7 +14,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - i; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
